9d837ae561cfe18bb14e5b1f35348fe66b4ffc72
luoyunchong <luoyunchong@foxmail.com>
2020-01-08 18:26:13 +0800

    添加项目文件。

---
+++ src/DotNetCore.CAP.MySql.FreeSql/CAP.MySqlCapOptionsExtension.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) .NET Core Community. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using DotNetCore.CAP.MySql;
+using DotNetCore.CAP.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+// ReSharper disable once CheckNamespace
+namespace DotNetCore.CAP
+{
+    internal class MySqlCapOptionsExtension : ICapOptionsExtension
+    {
+        private readonly Action<MySqlOptions> _configure;
+
+        public MySqlCapOptionsExtension(Action<MySqlOptions> configure)
+        {
+            _configure = configure;
+        }
+
+        public void AddServices(IServiceCollection services)
+        {
+            services.AddSingleton<CapStorageMarkerService>();
+            services.AddSingleton<IDataStorage, MySqlDataStorage>();
+            
+            services.TryAddSingleton<IStorageInitializer, MySqlStorageInitializer>();
+            services.AddTransient<ICapTransaction, MySqlCapTransaction>();
+
+            //Add MySqlOptions
+            services.Configure(_configure);
+            services.AddSingleton<IConfigureOptions<MySqlOptions>, ConfigureMySqlOptions>();
+        } 
+    }
+}
